13ffeb11735e6a5dff6f386105347b1575be22ef
Autumn Valenta <autumn@discoverywritten.com>
2025-09-23 16:32:28 -0600

    Fix use of inert=false

--- docs/js/accessibility.js
+++ docs/js/accessibility.js
@@ -107,8 +107,10 @@ const ACCESSIBILITY = {
 
 // Accessibility: Mutations
 function accessPlayer(on=true) {
-  usePlayer = LARASTELLE.retain('usePlayer', on);;
-  document.querySelector('.player').setAttribute('inert', !on);
+  usePlayer = LARASTELLE.retain('usePlayer', on);
+  const player = document.querySelector('.player');
+  if (on) player.setAttribute('inert', true);
+  else player.removeAttribute('inert');
 }
 function accessFocus(on=true) {
   useFocus = LARASTELLE.retain('useFocus', on);
